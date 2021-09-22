88d38c41f606cf617378cdbefe8865ec3cad2472
lambdanadasomething <59504808+lambdanadasomething@users.noreply.github.com>
2021-09-22 18:07:35 +0000

    Add sample Spring boot application code

---
+++ springboottest/src/main/java/com/lambdanada/springboottest/repo/UserRepository.java
@@ -0,0 +1,14 @@
+package com.lambdanada.springboottest.repo;
+
+import java.util.List;
+
+import com.lambdanada.springboottest.model.WebsiteUser;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<WebsiteUser, Long> {
+    List<WebsiteUser> findByName(@Param("name") String name);
+}
